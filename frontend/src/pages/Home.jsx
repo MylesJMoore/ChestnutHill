05a71bbde2aa6499329002b4399ea5ed8df244f1
MylesJMoore <Mylesjmoore@gmail.com>
2025-04-22 15:44:07 -0700

    We have a styled frontend!!

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,38 +1,10 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import PostList from "../components/PostList";
+import Layout from "../components/Layout";
 
-const Home = () => {
-  const [posts, setPosts] = useState([])
-
-  useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/posts')
-      .then(res => setPosts(res.data))
-      .catch(err => console.error(err))
-  }, [])
-
+export default function Home() {
   return (
-    <div className="p-6 text-white">
-      <h1 className="text-2xl font-bold mb-4">Chestnut Hill Posts</h1>
-      {posts.length === 0 ? (
-        <p>No posts found.</p>
-      ) : (
-        <ul className="space-y-4">
-          {posts.map(post => (
-            <li key={post.id} className="bg-gray-800 p-4 rounded shadow">
-              <p>{post.content}</p>
-              {post.image && (
-                <img
-                  src={`http://127.0.0.1:8000/storage/${post.image}`}
-                  alt="Post image"
-                  className="mt-2 rounded"
-                />
-              )}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  )
+    <Layout>
+      <PostList />
+    </Layout>
+  );
 }
-
-export default Home
