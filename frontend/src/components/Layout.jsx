acb6f1de2a0dff8048e8c70e3e8b816f18138b16
MylesJMoore <Mylesjmoore@gmail.com>
2025-06-04 22:45:39 -0700

    Fixed logout route and redirect back to login screen when you hit logout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,21 +1,12 @@
-import { useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import RightSidebar from "./RightSidebar";
 
 export default function Layout({ children, onLogout }) {
-  const navigate = useNavigate();
-
-  const handleLogoutClick = (e) => {
-    e.preventDefault();
-    onLogout();
-    navigate("/login");
-  };
-
   return (
     <div className="flex min-h-screen bg-white text-black">
-      {/* Left Sidebar (Replaced with component) */}
+      {/* Left Sidebar */}
       <aside className="w-64 border-r border-gray-200 p-4">
-        <Sidebar onLogout={handleLogoutClick} />
+        <Sidebar onLogout={onLogout} />
       </aside>
 
       {/* Main Content */}
