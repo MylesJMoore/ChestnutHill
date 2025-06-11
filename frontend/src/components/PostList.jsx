5d07286df4e8947e1da707e3b00dd7676f5199d8
MylesJMoore <Mylesjmoore@gmail.com>
2025-06-10 19:01:09 -0700

    Crushed it today. Added comment, respost, like, save icons. Fixed some middleware authentication things. Posts now show visually if they are liked or saved!!

--- frontend/src/components/PostList.jsx
+++ frontend/src/components/PostList.jsx
@@ -1,20 +1,32 @@
 import { useEffect, useState } from "react";
 import PostItem from "./PostItem";
+import { fetchWithAuth } from "../utils/api";
 
 export default function PostList() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true); // loading state
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/posts")
-      .then((res) => res.json())
-      .then((data) => setPosts(data))
-      .catch((err) => console.error("Error loading posts", err));
+    async function loadPosts() {
+      try {
+        const data = await fetchWithAuth("/posts"); // Get Authenticated requests
+        setPosts(data);
+      } catch (err) {
+        console.error("Error loading posts", err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadPosts();
   }, []);
 
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4 text-green-800">Chestnut Hill Posts</h1>
-      {posts.length > 0 ? (
+      {loading ? (
+        <p className="text-gray-600">Loading...</p>
+      ) : posts.length > 0 ? (
         posts.map((post) => <PostItem key={post.id} post={post} />)
       ) : (
         <p className="text-gray-600">No posts found.</p>
