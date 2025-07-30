7470f40e491cb6da4b24af8445939c0d9d40a316
MylesJMoore <Mylesjmoore@gmail.com>
2025-07-29 21:27:12 -0700

    Added ability to create posts. PHASE 2 DONE!!

--- frontend/src/components/PostList.jsx
+++ frontend/src/components/PostList.jsx
@@ -1,29 +1,34 @@
 import { useEffect, useState } from "react";
 import PostItem from "./PostItem";
+import CreatePost from "./CreatePost";
 import { fetchWithAuth } from "../utils/api";
 
 export default function PostList() {
   const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true); // loading state
+  const [loading, setLoading] = useState(true);
+
+  const loadPosts = async () => {
+    try {
+      const data = await fetchWithAuth("/posts");
+      setPosts(data);
+    } catch (err) {
+      console.error("Error loading posts", err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    async function loadPosts() {
-      try {
-        const data = await fetchWithAuth("/posts"); // Get Authenticated requests
-        setPosts(data);
-      } catch (err) {
-        console.error("Error loading posts", err);
-      } finally {
-        setLoading(false);
-      }
-    }
-
     loadPosts();
   }, []);
 
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4 text-green-800">Chestnut Hill Posts</h1>
+
+      {/* Post creation input */}
+      <CreatePost onPostCreated={loadPosts} />
+
       {loading ? (
         <p className="text-gray-600">Loading...</p>
       ) : posts.length > 0 ? (
