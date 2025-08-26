fc187ba87aee5437ac193b491ca526e183b4dcbd
MylesJMoore <Mylesjmoore@gmail.com>
2025-08-26 11:54:53 -0700

    SSM confirmed setup

--- infra/terraform/envs/dev/outputs.tf
+++ infra/terraform/envs/dev/outputs.tf
@@ -4,3 +4,10 @@ output "private_subnets" { value = module.vpc.private_subnets }
 output "ecr_backend_url" { value = module.ecr_backend.repository_url }
 output "ecr_frontend_url" { value = module.ecr_frontend.repository_url }
 output "s3_images_bucket" { value = module.s3_images.s3_bucket_id }
+# SSM parameter NAMES only (not values)
+output "ssm_param_names" {
+  value = [
+    aws_ssm_parameter.app_key.name,
+    aws_ssm_parameter.db_password.name,
+  ]
+}
